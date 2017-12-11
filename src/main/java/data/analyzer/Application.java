d312f8b2863d4eef411264448ff6cacb6773616e
apaczuski00 <apaczuski00@gmail.com>
2017-12-11 22:29:27 +0100

    first comm

--- src/main/java/data/analyzer/Application.java
+++ src/main/java/data/analyzer/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AnalyzerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AnalyzerApplication.class, args);
-		System.out.println("Work");
+		
+		SpringApplication.run(Application.class, args);
 	}
 }
